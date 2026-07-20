02017c22952425474890e9ea8041fba3f037d550
ksjxjcmxncj-maker <ksjxjcmxncj@gmail.com>
2026-07-20 11:29:44 +0700

    fix(Utils): replace raw Thread with ScheduledExecutorService thread pool [HIGH]

--- server/src/util/Utils.java
+++ server/src/util/Utils.java
@@ -1,16 +1,21 @@
 // OBITO
 package util;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class Utils {
 
+    private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(4);
+
     public static void setTimeout(Runnable runnable, int delay) {
-        new Thread(() -> {
+        scheduler.schedule(() -> {
             try {
-                Thread.sleep(delay);
                 runnable.run();
             } catch (Exception e) {
                 System.err.println(e);
             }
-        }).start();
+        }, delay, TimeUnit.MILLISECONDS);
     }
 }
